5b91f81fa1f28aa96d6faabfd4f2ff07e7ef1a67
leonhaft <929397+leonhaft@users.noreply.github.com>
2021-09-10 19:51:22 +0800

    修正删除查找元素不正确

--- qqlive.js
+++ qqlive.js
@@ -1,59 +1,66 @@
 (function () {
-    var inte = setInterval(lookElements, 1000);
-    var loopCount = 0;
-    var successDiv = false;
-
-    function lookElements() {
-        if (successDiv == false) {
-            findSickDiv();
-        }
-
-        loopCount++;
-        if (successDiv || loopCount > 20) {
-            clearInte();
-        }
+  var inte = setInterval(lookElements, 1000);
+  var loopCount = 0;
+  var successDiv = false;
+  var sickBackgroundImageRegex = /\w{1,}.jpg$/g;
+  function lookElements() {
+    if (successDiv == false) {
+      findSickDiv();
     }
 
-    function clearInte() {
-        clearInterval(inte);
+    loopCount++;
+    if (successDiv || loopCount > 20) {
+      clearInte();
+    }
+  }
+
+  function clearInte() {
+    clearInterval(inte);
+  }
+
+  function findSickLink(container) {
+    var continueLinkRemove;
+    for (var i = 0; i < container.childNodes.length; i++) {
+      var currentNode = container.childNodes[i];
+      if (currentNode.nodeName === "A") {
+        var href = currentNode.getAttribute("href");
+        if (href.length == 0) {
+          continueLinkRemove = currentNode;
+          break;
+        }
+      }
     }
 
-    function findSickLink(container) {
-        var continueLinkRemove;
-        for (var i = 0; i < container.childNodes.length; i++) {
-            var currentNode = container.childNodes[i];
-            if (currentNode.nodeName === 'A') {
-                continueLinkRemove = currentNode;
-                break;
-            }
-        }
+    if (continueLinkRemove) {
+      continueLinkRemove.remove();
+      console.log("恶心链接删除成功");
+    }
+  }
 
-        if (continueLinkRemove) {
-            continueLinkRemove.remove();
-            console.log("恶心链接删除成功");
+  function findSickDiv() {
+    var divs = document.getElementsByTagName("div");
+    console.log(divs.length);
+    var continueRemove;
+    for (var i = 0; i < divs.length; i++) {
+      var style = window.getComputedStyle(divs[i]);
+      if (style.backgroundImage) {
+        var bimg = style.backgroundImage;
+        if (
+          bimg.indexOf("qiecdn") > 0 &&
+          bimg.indexOf("upload") > 0 &&
+          bimg.match(sickBackgroundImageRegex)
+        ) {
+          continueRemove = divs[i];
+          break;
         }
+      }
     }
 
-    function findSickDiv() {
-        var divs = document.getElementsByTagName('div');
-        console.log(divs.length)
-        var continueRemove;
-        for (var i = 0; i < divs.length; i++) {
-            var style = window.getComputedStyle(divs[i]);
-            if (style.backgroundImage) {
-                if (style.backgroundImage.indexOf('qiecdn') > 0) {
-                    continueRemove = divs[i];
-                    break;
-                }
-            }
-        }
-
-        if (continueRemove) {
-            continueRemove.style = null;
-            console.log("烦人背景删除成功");
-            successDiv = true;
-            findSickLink(continueRemove);
-        }
+    if (continueRemove) {
+      continueRemove.style = null;
+      console.log("烦人背景删除成功");
+      successDiv = true;
+      findSickLink(continueRemove);
     }
-
+  }
 })();
